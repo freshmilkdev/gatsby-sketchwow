9c7f7a7110fe60709d4906bebceea5277c2811c3
Max <max.ruchkin@gmail.com>
2023-11-28 17:54:19 +0200

    cards fully responsive all resolutions

--- src/components/SayHelloSection/SayHelloSection.js
+++ src/components/SayHelloSection/SayHelloSection.js
@@ -82,19 +82,14 @@ export const SayHelloSection = () => (<section>
                         quality={95}
                         alt=""
                     />}
-                           heading={'No More Death-By-Power Point…  Like Ever'}
+                           heading={'How Does SketchWow Animate Work?'}
                            content={<>
-                               <p>Plus, adding SketchWow Animate makes SkechWow even MORE fun to use. <br/>
-                                   <span style={{fontStyle: 'italic'}}>(... never thought that was possible, until we
-                                           proved ourselves wrong!)</span>
-                               </p>
+                               <p>Once you purchase the SketchWow Animate add-on, a new “Animate” button will be enabled
+                                   in SketchWow on the top toolbar.</p>
 
-                               <p>There are dozens of presentation and animation tools out there… but as you’ll
-                                   soon <br/> find out, there’s NOTHING like SketchWow Animate!</p>
+                               <p>Click it… that’s when the magic happens!</p>
                                <p>
-                                   Keep reading to see some of the unique, eye-popping animations you can create
-                                   only <br/>
-                                   <span style={{fontWeight: 600}}><u>using SketchWow Animate!</u></span>
+                                   Next, you can add animation and movement to any element, shape, text, page or imported image.
                                </p>
                            </>}
                 />
