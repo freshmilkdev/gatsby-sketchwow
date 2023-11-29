e7edf33526f5aaf26e3ad31b1ce39e7a7837eff4
Max <max.ruchkin@gmail.com>
2023-11-29 18:10:06 +0200

    watch your designs section

--- src/components/WatchDesignsSection/WatchDesignsSection.js
+++ src/components/WatchDesignsSection/WatchDesignsSection.js
@@ -2,6 +2,8 @@ import React from 'react';
 import {LineHeading} from "../LineHeading/LineHeading";
 import {Container} from "../Layout/Container/Container";
 import {SectionText} from "../SectionText/SectionText";
+import * as styles from "./WatchDesignsSection.module.css";
+import {StaticImage} from "gatsby-plugin-image";
 
 
 export const WatchDesignsSection = () => (
@@ -13,10 +15,40 @@ export const WatchDesignsSection = () => (
             <SectionText>You get all the standard effects that you’d expect to see (Appear, Fade Out, Push, Pull…etc)
                 plus a few other <br/>
                 "magnetic" effects that are exclusive to SketchWow animate, including...</SectionText>
-
+            <div className={styles.row}>
+                <StaticImage
+                    className={styles.left}
+                    src="../../images/watch-left.png"
+                    loading="lazy"
+                    width={620}
+                    quality={98}
+                    alt=""
+                />
+                <StaticImage
+                    src="../../images/watch-right.png"
+                    loading="lazy"
+                    width={672}
+                    quality={98}
+                    alt=""
+                />
+            </div>
             <SectionText>
-                <span>Is that cool or what?</span><br/>
-                SketchWow Animate also includes a few unique Text Effects too (Typewriter and Text Jiggle) like this...
+                <p className={'font-30 font-extrabold'}>Is that cool or what?</p><br/>
+                <p>SketchWow Animate also includes a few unique Text Effects too (Typewriter and Text Jiggle) like
+                    this...</p>
+            </SectionText>
+            <div className={styles.jingle}>
+                <StaticImage
+                    className={styles.left}
+                    src="../../images/watch-jingle.png"
+                    loading="lazy"
+                    width={1040}
+                    quality={98}
+                    alt=""
+                />
+            </div>
+            <SectionText>
+                <p>Just select an animation Effect and watch those words really POP!</p>
             </SectionText>
         </Container>
     </section>
