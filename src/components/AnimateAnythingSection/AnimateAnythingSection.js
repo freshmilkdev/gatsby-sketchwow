55741c5c81aa062b11aaac5bdb3ed75afe9e30d6
Max <max.ruchkin@gmail.com>
2023-11-29 15:50:40 +0200

    clicks section

--- src/components/AnimateAnythingSection/AnimateAnythingSection.js
+++ src/components/AnimateAnythingSection/AnimateAnythingSection.js
@@ -2,12 +2,13 @@ import React from 'react';
 import {WaveHeaderSection} from "../WaveHeaderSection/WaveHeaderSection";
 import {LineHeading} from "../LineHeading/LineHeading";
 import * as styles from './AnimateAnythingSection.module.css'
+import {StaticImage} from "gatsby-plugin-image";
+import {TwoClicks} from "./TwoClicks";
 
-export const AnimateAnythingSection = () => (
-    <WaveHeaderSection color={'green'}>
+export const AnimateAnythingSection = () => (<WaveHeaderSection color={'green'}>
         <LineHeading lineColor='green'>
             <span>2 Clicks = <u>Animate Anything</u></span>
         </LineHeading>
         <p className={`${styles.afterHeading} font-24 font-medium text-center`}>Yup. It's that easy!</p>
-    </WaveHeaderSection>
-);
+        <TwoClicks/>
+    </WaveHeaderSection>);
