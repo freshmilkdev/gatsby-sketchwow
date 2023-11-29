a5878fe69bbb91168d342d5c752a2089e0dbae17
Max <max.ruchkin@gmail.com>
2023-11-29 17:07:23 +0200

    2 clicks refactored

--- src/components/AnimateAnythingSection/AnimateAnythingSection.js
+++ src/components/AnimateAnythingSection/AnimateAnythingSection.js
@@ -3,7 +3,7 @@ import {WaveHeaderSection} from "../WaveHeaderSection/WaveHeaderSection";
 import {LineHeading} from "../LineHeading/LineHeading";
 import * as styles from './AnimateAnythingSection.module.css'
 import {StaticImage} from "gatsby-plugin-image";
-import {TwoClicks} from "./TwoClicks";
+
 
 export const AnimateAnythingSection = () => (
     <WaveHeaderSection color={'green'}>
@@ -11,30 +11,54 @@ export const AnimateAnythingSection = () => (
             <span>2 Clicks = <u>Animate Anything</u></span>
         </LineHeading>
         <p className={`${styles.afterHeading} font-24 font-medium text-center`}>Yup. It's that easy!</p>
-        <TwoClicks/>
-        <div className={styles.appScreenshots}>
-            <StaticImage
-                objectFit={'contain'}
-                src="../../images/2clicks-left.png"
-                loading="lazy"
-                width={753}
-                quality={90}
-                alt=""
-            />
-            <div className={styles.rightWrapper}>
+
+        <div className={`${styles.clicks} font-24 font-bold`}>
+            <div>
+                <div className={styles.click}>
+                    <StaticImage
+                        className={styles.image}
+                        src="../../images/click1.png"
+                        loading="lazy"
+                        width={149}
+                        quality={95}
+                        alt=""
+                    />
+                    <h5>Highlight An Element On The Canvas</h5>
+                </div>
                 <StaticImage
-                    className={styles.rightImageWrapper}
-                    src="../../images/2clicks-right.png"
+                    src="../../images/2clicks-left.png"
                     loading="lazy"
-                    width={715}
-                    quality={100}
+                    width={753}
+                    quality={90}
                     alt=""
                 />
             </div>
+            <div className={styles.rightCol}>
+                <div className={styles.click}>
+                    <StaticImage
+                        className={styles.image}
+                        src="../../images/click2.png"
+                        loading="lazy"
+                        width={151}
+                        quality={95}
+                        alt=""
+                    />
+                    <h5>Choose Your Animation Effect</h5>
+                </div>
+                <div className={styles.rightWrapper}>
+                    <StaticImage
+                        src="../../images/2clicks-right.png"
+                        loading="lazy"
+                        width={715}
+                        quality={100}
+                        alt=""
+                    />
+                </div>
+            </div>
         </div>
+
         <div className={styles.bubble}>
             <StaticImage
-                className={styles.rightImageWrapper}
                 src="../../images/2clicks-right-bubble.png"
                 loading="lazy"
                 width={396}
