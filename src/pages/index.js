eb01f51fd7922d44c41e5cf04bcf03f41e66741b
max <max.ruchkin@gmail.com>
2023-12-03 12:37:43 +0200

    custom font loader

--- src/pages/index.js
+++ src/pages/index.js
@@ -43,15 +43,19 @@ const IndexPage = () => (
 export const Head = () => (
     <>
         <Seo title="Home"/>
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin/>
+            <style>
+                    @import url('https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800;900&display=swap');
+            </style>
+        {/*<link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link rel="preload" as="style"
-              href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800;900&display=swap"/>
-        <link rel="stylesheet" media="print" onLoad="this.onload=null;this.removeAttribute('media');"
-              href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800;900&display=swap"/>
-        <noscript>
-            <link rel="stylesheet"
-                  href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800;900&display=swap"/>
-        </noscript>
+              href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800;900&display=swap"/>*/}
+        {/*<link rel="stylesheet" media="print" onLoad="this.onload=null;this.removeAttribute('media');"*/}
+        {/*      href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800;900&display=swap"/>*/}
+        {/*<noscript>*/}
+        {/*    <link rel="stylesheet"*/}
+        {/*          href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800;900&display=swap"/>*/}
+        {/*</noscript>*/}
     </>
 )
 
