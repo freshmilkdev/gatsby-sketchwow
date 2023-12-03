d650f19c63f257aa56f97792833761ad9d57a844
max <max.ruchkin@gmail.com>
2023-12-03 12:54:12 +0200

    custom font loader

--- src/pages/index.js
+++ src/pages/index.js
@@ -43,19 +43,20 @@ const IndexPage = () => (
 export const Head = () => (
     <>
         <Seo title="Home"/>
-            <style>
-                    @import url('https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800;900&display=swap');
-            </style>
-        {/*<link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link rel="preload" as="style"
-              href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800;900&display=swap"/>*/}
-        {/*<link rel="stylesheet" media="print" onLoad="this.onload=null;this.removeAttribute('media');"*/}
-        {/*      href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800;900&display=swap"/>*/}
-        {/*<noscript>*/}
-        {/*    <link rel="stylesheet"*/}
-        {/*          href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800;900&display=swap"/>*/}
-        {/*</noscript>*/}
+        <link rel="preconnect" href="https://fonts.googleapis.com"/>
+        <link rel="preconnect" href="https://fonts.gstatic.com"/>
+        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800;900&display=swap"
+              rel="stylesheet"/>
+        {/* <link rel="preconnect" href="https://fonts.googleapis.com" crossOrigin="true"/>
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true"/>
+        <link rel="preload"
+              href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800;900&display=swap" />*/}
+        <link rel="stylesheet" media="print" onLoad="this.onload=null;this.removeAttribute('media');"
+              href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800;900&display=swap"/>
+        <noscript>
+            <link rel="stylesheet"
+                  href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800;900&display=swap"/>
+        </noscript>
     </>
 )
 
