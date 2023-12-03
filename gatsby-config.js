ef0ed61019a5c02a18e27e6e77b9d9b781bf071a
max <max.ruchkin@gmail.com>
2023-12-03 11:30:08 +0200

    another font plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -48,18 +48,15 @@ module.exports = {
             },
         },
         {
-            resolve: `gatsby-omni-font-loader`,
+            resolve: `gatsby-plugin-google-fonts-v2`,
             options: {
-                mode: "async",
-                enableListener: true,
-                preconnect: [`https://fonts.gstatic.com`],
-                web: [
+                fonts: [
                     {
-                        name: `Open Sans`,
-                        file: `https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800;900&display=swap`,
+                        family: 'Inter',
+                        weights: ['300', '400', '500', '600', '700', '800', '400']
                     },
-                ],
-            },
-        },
+                ]
+            }
+        }
     ],
 }
