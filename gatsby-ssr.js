efd0aeb5f2c43fc22b30b0bf39cff462df289991
max <max.ruchkin@gmail.com>
2023-12-03 13:14:52 +0200

    setPostBodyComponents without

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -8,19 +8,6 @@ const React = require("react");
 /**
  * @type {import('gatsby').GatsbySSR['onRenderBody']}
  */
-exports.onRenderBody = ({setHtmlAttributes, setPostBodyComponents}) => {
-    setHtmlAttributes({lang: `en`}),
-        setPostBodyComponents([
-            <link rel="preconnect" href="https://fonts.googleapis.com"/>,
-            <link rel="preconnect" href="https://fonts.gstatic.com"/>,
-            <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800;900&display=swap"
-                  rel="stylesheet"/>,
-            // <link rel="stylesheet" media="print" onLoad="this.onload=null;this.removeAttribute('media');"
-            //       href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800;900&display=swap"/>,
-            // <noscript>
-            //     <link rel="stylesheet"
-            //           href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800;900&display=swap"/>
-            // </noscript>
-
-        ])
+exports.onRenderBody = ({setHtmlAttributes}) => {
+    setHtmlAttributes({lang: `en`})
 }
