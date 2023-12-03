1adb697ca62b72d77316304e8b8492f1b8d375d0
max <max.ruchkin@gmail.com>
2023-12-03 11:04:29 +0200

    gatsby-plugin-google-fonts-v2 test

--- gatsby-config.js
+++ gatsby-config.js
@@ -46,6 +46,17 @@ module.exports = {
                 display: `minimal-ui`,
                 icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
             },
+        },
+        {
+            resolve: `gatsby-plugin-google-fonts-v2`,
+            options: {
+                fonts: [
+                    {
+                        family: 'Inter',
+                        weights: ['300', '400', '500', '600', '700', '800', '900']
+                    }
+                ]
+            }
         }
     ],
 }
