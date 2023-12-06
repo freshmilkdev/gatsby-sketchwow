eabc095dc7b54e23335312ecadce820f7ccb7f4d
Max <max.ruchkin@gmail.com>
2023-12-06 20:12:09 +0200

    watch section videos

--- src/components/WatchDesignsSection/WatchDesignsSection.js
+++ src/components/WatchDesignsSection/WatchDesignsSection.js
@@ -5,8 +5,9 @@ import {SectionText} from "../SectionText/SectionText";
 import * as styles from "./WatchDesignsSection.module.css";
 import {StaticImage} from "gatsby-plugin-image";
 import ShakeVideo from "../../videos/Shake-animate.mp4"
-import DrawVideo from "../../videos/Draw-animate.mp4"
+import DrawVideo from "../../videos/Draw-animate2.mp4"
 import TextVideo from "../../videos/Text-animate.mp4"
+
 export const WatchDesignsSection = () => (
     <section className={styles.section}>
         <Container>
@@ -67,18 +68,28 @@ export const WatchDesignsSection = () => (
                 <p>SketchWow Animate also includes a few unique Text Effects too (Typewriter and Text Jiggle) like
                     this...</p>
             </SectionText>
-            <div className={styles.jingle}>
-                <video width="100%" loop autoPlay muted>
-                    <source src={ShakeVideo} type="video/mp4"/>
-                </video>
-                <StaticImage
-                    className={styles.left}
-                    src="../../images/btn-jiggle.png"
-                    loading="lazy"
-                    width={264}
-                    quality={98}
-                    alt=""
-                />
+            <div className={styles.jiggle}>
+                <div className={styles.inner}>
+                    <video width="100%" loop autoPlay muted>
+                        <source src={TextVideo} type="video/mp4"/>
+                    </video>
+                    <StaticImage
+                        className={styles.btnJiggle}
+                        src="../../images/btn-jiggle.png"
+                        loading="lazy"
+                        width={264}
+                        quality={98}
+                        alt=""
+                    />
+                    <StaticImage
+                        className={styles.pointerJiggle}
+                        src="../../images/pointer-left.png"
+                        loading="lazy"
+                        width={76}
+                        quality={98}
+                        alt=""
+                    />
+                </div>
             </div>
             <SectionText>
                 <p>Just select an animation Effect and watch those words really POP!</p>
