0a81dff0e0f4c82bf0d040404fcdd548edbb80b6
Max <max.ruchkin@gmail.com>
2023-12-19 20:16:08 +0200

    warning fix

--- src/components/Hero/HeroSubheading.js
+++ src/components/Hero/HeroSubheading.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import * as styles from './Hero.module.css';
-import SVG from "../../assets/svg/underline.inline.svg";
+import UnderlineSVG from "../../assets/svg/underline.inline.svg";
 
 export const HeroSubheading = ({text}) => (
     <h3 className={styles.subheading}>
         Imagine If... In 2 Mouse Clicks, You Can <span className={styles.underline}>
             Instantly
-        <SVG className={styles.underlineImage}/>
+        <UnderlineSVG className={styles.underlineImage}/>
         </span> Add Eye-Catching Animations, Effects & Motion To
         ALL Your
         SketchWow Designs
